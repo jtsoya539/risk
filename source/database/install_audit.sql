e85d360870b587680432e4151bcb2484a36a8d86
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-02-03 09:34:41 -0300

    Ajustes en script de generación de auditoría

--- source/database/install_audit.sql
+++ source/database/install_audit.sql
@@ -1,3 +1,5 @@
+set serveroutput on
+
 DECLARE
   CURSOR cr_tablas IS
     SELECT lower(table_name) tabla
@@ -5,12 +7,16 @@ DECLARE
      WHERE lower(table_name) LIKE 't_%';
 BEGIN
   FOR t IN cr_tablas LOOP
-    dbms_output.put_line('Generating audit columns...');
+    dbms_output.put_line('Generating audit columns for table ' ||
+                         upper(t.tabla) || '...');
     dbms_output.put_line('---------------------------');
     k_auditoria.p_generar_campos_auditoria(i_tabla => t.tabla);
-    dbms_output.put_line('Generating audit triggers...');
+    dbms_output.put_line('Generating audit triggers for table ' ||
+                         upper(t.tabla) || '...');
     dbms_output.put_line('---------------------------');
     k_auditoria.p_generar_trigger_auditoria(i_tabla => t.tabla);
   END LOOP;
 END;
 /
+
+set serveroutput off
