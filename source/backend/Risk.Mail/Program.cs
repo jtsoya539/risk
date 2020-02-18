4d0d5b3fe2391539f6cbc9cbbc93d80ce9fa79a7
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-02-18 13:34:00 -0300

    Agregar proyectos para mensajería por Mail y SMS

---
+++ source/backend/Risk.Mail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Risk.Mail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
