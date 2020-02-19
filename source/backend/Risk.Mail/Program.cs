2ea20196c7dd31c4b1fe59ac6a0888136d815270
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-02-19 13:07:16 -0300

    Versión inicial de Risk.Mail

--- source/backend/Risk.Mail/Program.cs
+++ source/backend/Risk.Mail/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using MailKit.Net.Proxy;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace Risk.Mail
 {
@@ -7,6 +10,35 @@ namespace Risk.Mail
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Información RISK", "informacion@risk.com.py"));
+            message.To.Add(new MailboxAddress("Javier", "javier.meza@rama.com.py"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                // client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.ProxyClient = new HttpProxyClient("svr0062", 8080);
+
+                client.Connect("smtp.gmail.com", 465, true);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("javier.meza.py@fpuna.edu.py", "rama.1212");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
     }
 }
