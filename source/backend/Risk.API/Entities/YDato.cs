0bc1cb4eecf326d5bf8edbbbec668ae352ea8cad
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-03-06 11:39:48 -0300

    Agregar nombres de propiedades Json

--- source/backend/Risk.API/Entities/YDato.cs
+++ source/backend/Risk.API/Entities/YDato.cs
@@ -1,7 +1,10 @@
+using Newtonsoft.Json;
+
 namespace Risk.API.Entities
 {
     public class YDato
     {
+        [JsonProperty("dato")]
         public string Dato { get; set; }
     }
 }
--- source/backend/Risk.API/Entities/YRespuesta.cs
+++ source/backend/Risk.API/Entities/YRespuesta.cs
@@ -1,11 +1,18 @@
+using Newtonsoft.Json;
+
 namespace Risk.API.Entities
 {
     public class YRespuesta<T>
     {
+        [JsonProperty("codigo")]
         public string Codigo { get; set; }
+        [JsonProperty("mensaje")]
         public string Mensaje { get; set; }
+        [JsonProperty("mensaje_bd")]
         public string MensajeBd { get; set; }
+        [JsonProperty("lugar")]
         public string Lugar { get; set; }
+        [JsonProperty("datos")]
         public T Datos { get; set; }
     }
 }
