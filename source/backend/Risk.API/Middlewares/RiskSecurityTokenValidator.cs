4f00115c6150d8925cda8ec8ac634ac479623025
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-03-10 15:18:55 -0300

    Cambiar namespace de RiskSecurityTokenValidator

--- source/backend/Risk.API/Middlewares/RiskSecurityTokenValidator.cs
+++ source/backend/Risk.API/Middlewares/RiskSecurityTokenValidator.cs
@@ -2,8 +2,9 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using Risk.API.Entities;
+using Risk.API.Services;
 
-namespace Risk.API.Services
+namespace Risk.API.Middlewares
 {
     public class RiskSecurityTokenValidator : ISecurityTokenValidator
     {
