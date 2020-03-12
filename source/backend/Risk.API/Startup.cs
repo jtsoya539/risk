8c15a00712908dd7af3b854604a668f9aa77ece6
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-03-12 14:08:31 -0300

    Agregar servers en Swagger

--- source/backend/Risk.API/Startup.cs
+++ source/backend/Risk.API/Startup.cs
@@ -142,6 +142,7 @@ namespace Risk.API
                     }
                 });
                 c.OperationFilter<RiskApplicationKeyHeaderOperationFilter>();
+                c.DocumentFilter<RiskDocumentFilter>();
             });
         }
 
