871480b00138fe3b7a190f152b4a57acd6c612ff
Javier Augusto <javier.meza.py@gmail.com>
2020-03-27 22:58:24 -0400

    Agregar script para generar documentación de Base de Datos

---
+++ source/database/generate_docs.sql
@@ -0,0 +1,95 @@
+/*
+--------------------------------- MIT License ---------------------------------
+Copyright (c) 2019 jtsoya539
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+-------------------------------------------------------------------------------
+*/
+
+set feedback off
+set define off
+
+prompt ###################################
+prompt #   _____   _____   _____  _  __  #
+prompt #  |  __ \ |_   _| / ____|| |/ /  #
+prompt #  | |__) |  | |  | (___  | ' /   #
+prompt #  |  _  /   | |   \___ \ |  <    #
+prompt #  | | \ \  _| |_  ____) || . \   #
+prompt #  |_|  \_\|_____||_____/ |_|\_\  #
+prompt #                                 #
+prompt #          Proyecto RISK          #
+prompt #            jtsoya539            #
+prompt ###################################
+
+prompt
+prompt ===================================
+prompt Generacion de docs iniciada
+prompt ===================================
+prompt
+
+prompt
+prompt Eliminando docs...
+prompt -----------------------------------
+prompt
+plugin plsqldoc delete;
+
+prompt
+prompt Generando docs de secuencias...
+prompt -----------------------------------
+prompt
+
+prompt
+prompt Generando docs de tablas...
+prompt -----------------------------------
+prompt
+
+prompt
+prompt Generando docs de types...
+prompt -----------------------------------
+prompt
+
+prompt
+prompt Generando docs de paquetes...
+prompt -----------------------------------
+prompt
+plugin plsqldoc generate k_auditoria;
+plugin plsqldoc generate k_autenticacion;
+plugin plsqldoc generate k_error;
+plugin plsqldoc generate k_html;
+plugin plsqldoc generate k_mensajeria;
+plugin plsqldoc generate k_servicio;
+plugin plsqldoc generate k_sistema;
+plugin plsqldoc generate k_util;
+
+prompt
+prompt Generando docs de triggers...
+prompt -----------------------------------
+prompt
+
+prompt
+prompt Generando index...
+prompt -----------------------------------
+prompt
+plugin plsqldoc rebuild;
+
+prompt
+prompt ===================================
+prompt Generacion de docs finalizada
+prompt ===================================
+prompt
