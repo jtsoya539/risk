b17c3ceefd0c39fe8515f3211e02f3b1d2879262
jtsoya539 <javier.meza.py@gmail.com>
2020-05-28 19:41:21 -0400

    Definir tiempo de expiración de tokens por aplicación

--- source/database/scripts/ins_t_aplicaciones.sql
+++ source/database/scripts/ins_t_aplicaciones.sql
@@ -1,7 +1,8 @@
 prompt Importing table t_aplicaciones...
 set feedback off
 set define off
-insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL)
-values ('TEST', 'RISK TEST', 'W', 'S', 'KM1gROAnPUAZykrHcOOJRAC1jkcnyBVLRWXcRD3pjvQ=', 'Cliente HTTP para pruebas de Servicios Web', '0.1');
+
+insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN)
+values ('TEST', 'RISK TEST', 'W', 'S', 'KM1gROAnPUAZykrHcOOJRAC1jkcnyBVLRWXcRD3pjvQ=', 'Cliente HTTP para pruebas de Servicios Web', '0.1', 300, 5);
 
 prompt Done.
--- source/database/scripts/ins_t_parametros.sql
+++ source/database/scripts/ins_t_parametros.sql
@@ -1,11 +1,15 @@
 prompt Importing table t_parametros...
 set feedback off
 set define off
+
 insert into t_parametros (ID_PARAMETRO, DESCRIPCION, VALOR)
 values ('BASE_DATOS_PRODUCCION', 'Nombre de la Base de Datos del entorno de Producción', 'RISK');
 
 insert into t_parametros (ID_PARAMETRO, DESCRIPCION, VALOR)
 values ('CANTIDAD_MAXIMA_SESIONES_USUARIO', 'Cantidad máxima permitida de sesiones activas por usuario', '2');
+
+insert into t_parametros (ID_PARAMETRO, DESCRIPCION, VALOR)
+values ('CLAVE_VALIDACION_ACCESS_TOKEN', 'Clave de validación del Access Token', '9vVzzZbbUCcYE3cDnE+IVMrLF+8X8TPyK2cmC3Vu7M0=');
 
 insert into t_parametros (ID_PARAMETRO, DESCRIPCION, VALOR)
 values ('LONGITUD_MINIMA_CLAVE_ACCESO', 'Longitud mínima permitida para clave de acceso', '8');
@@ -29,6 +33,6 @@ insert into t_parametros (ID_PARAMETRO, DESCRIPCION, VALOR)
 values ('TIEMPO_EXPIRACION_REFRESH_TOKEN', 'Tiempo de expiración del Refresh Token en horas', '5');
 
 insert into t_parametros (ID_PARAMETRO, DESCRIPCION, VALOR)
-values ('CLAVE_VALIDACION_ACCESS_TOKEN', 'Clave de validación del Access Token', '9vVzzZbbUCcYE3cDnE+IVMrLF+8X8TPyK2cmC3Vu7M0=');
+values ('URL_SERVICIOS_PRODUCCION', 'URL base de los Servicios Web del entorno de Producción', 'http://localhost:5000');
 
 prompt Done.
