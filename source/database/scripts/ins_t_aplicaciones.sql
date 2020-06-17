d8cfc42f88d8b48f664a7da9e52cfb841ef3866d
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-06-17 10:26:19 -0400

    Agregar aplicación para mensajería

--- source/database/scripts/ins_t_aplicaciones.sql
+++ source/database/scripts/ins_t_aplicaciones.sql
@@ -4,4 +4,7 @@ set define off
 insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN)
 values ('TEST', 'RISK TEST', 'W', 'S', 'KM1gROAnPUAZykrHcOOJRAC1jkcnyBVLRWXcRD3pjvQ=', 'Cliente HTTP para pruebas de Servicios Web', '0.1', 3600, 5);
 
+insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN)
+values ('MSJ', 'RISK MENSAJERÍA', 'W', 'S', 'Va1eggcdH9F27TTuLFMzU6s4mt568v439ZOD+8dw9ig=', 'Servicio para envío de mensajes a los usuarios a través de Correo electrónico (E-mail) y Mensaje de texto (SMS)', '0.1', 86400, 10000);
+
 prompt Done.
