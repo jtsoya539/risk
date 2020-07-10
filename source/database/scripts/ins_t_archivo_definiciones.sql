2dd21b713d759578a3a49a4499758bdb7eac92fd
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-07-10 09:46:20 -0400

    Agregar definición de archivo para imágenes

--- source/database/scripts/ins_t_archivo_definiciones.sql
+++ source/database/scripts/ins_t_archivo_definiciones.sql
@@ -10,4 +10,7 @@ values ('T_CORREO_ADJUNTOS', 'ARCHIVO', 'Archivo adjunto', 10000000, 1, 'ID_CORR
 insert into t_archivo_definiciones (TABLA, CAMPO, DESCRIPCION, TAMANO_MAXIMO, ORDEN, NOMBRE_REFERENCIA, EXTENSIONES_PERMITIDAS)
 values ('T_FUENTES', 'ARCHIVO', 'Archivo de fuente', 10000000, 1, null, 'EXTENSION_FUENTE');
 
+insert into t_archivo_definiciones (TABLA, CAMPO, DESCRIPCION, TAMANO_MAXIMO, ORDEN, NOMBRE_REFERENCIA, EXTENSIONES_PERMITIDAS)
+values ('T_IMAGENES', 'ARCHIVO', 'Archivo de imagen', 10000000, 1, null, 'EXTENSION_IMAGEN');
+
 prompt Done.
