ae14d0d36538f24d2d19d23d747181f3435c5e81
jtsoya539 <javier.meza.py@gmail.com>
2020-07-24 10:07:01 -0400

    Configurar template de notificación push para aplicaciones móviles

--- source/database/scripts/ins_t_aplicaciones.sql
+++ source/database/scripts/ins_t_aplicaciones.sql
@@ -5,10 +5,10 @@ insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE,
 values ('WIN', 'WINDOWS', 'D', 'N', 'u0jVxK7vw3b4O1/pnJnFD5CzoY4lyvRfwC/yUfXscFk=', 'Aplicación para la plataforma Windows', '0.1', 900, 5, null, 'wns');
 
 insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
-values ('AND', 'ANDROID', 'M', 'N', 'azVd94zazPu/+q5ZHqoL1v6wccamHV3oWoALYWQK0Z8=', 'Aplicación móvil para la plataforma Android', '0.1', 900, 5, null, 'fcm');
+values ('AND', 'ANDROID', 'M', 'N', 'azVd94zazPu/+q5ZHqoL1v6wccamHV3oWoALYWQK0Z8=', 'Aplicación móvil para la plataforma Android', '0.1', 900, 5, '{"notification":{"title":"$(titulo)","body":"$(contenido)"}}', 'fcm');
 
 insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
-values ('IOS', 'IOS', 'M', 'N', '7C+o70uaWg7q4INnuzryHorFzbjNQOCWjrYZzWDQmLA=', 'Aplicación móvil para la plataforma iOS', '0.1', 900, 5, null, 'apns');
+values ('IOS', 'IOS', 'M', 'N', '7C+o70uaWg7q4INnuzryHorFzbjNQOCWjrYZzWDQmLA=', 'Aplicación móvil para la plataforma iOS', '0.1', 900, 5, '{"aps":{"alert":"$(contenido)"}}', 'apns');
 
 insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
 values ('MAIL', 'RISK MAIL', 'S', 'S', 'Hkru7cXdu6FdIqhA9Q4XJqTgW/pW651rDjlSyLczKwI=', 'Servicio para envío de mensajes a los usuarios a través de Correo electrónico (E-mail)', '0.1', 86400, 10000, null, null);
