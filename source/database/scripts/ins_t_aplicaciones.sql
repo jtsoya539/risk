943fe862819b807caa34ca12c14498daad9dadfe
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-08-05 08:46:32 -0400

    Registrar aplicación MSJ y eliminar MAIL, PUSH y SMS

--- source/database/scripts/ins_t_aplicaciones.sql
+++ source/database/scripts/ins_t_aplicaciones.sql
@@ -11,13 +11,7 @@ insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE,
 values ('IOS', 'IOS', 'M', 'S', '7C+o70uaWg7q4INnuzryHorFzbjNQOCWjrYZzWDQmLA=', 'Aplicación móvil para la plataforma iOS', '0.1', 900, 5, '{"aps":{"alert":"$(contenido)"}}', 'apns');
 
 insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
-values ('MAIL', 'RISK MAIL', 'S', 'S', 'Hkru7cXdu6FdIqhA9Q4XJqTgW/pW651rDjlSyLczKwI=', 'Servicio para envío de mensajes a los usuarios a través de Correo electrónico (E-mail)', '0.1', 86400, 10000, null, null);
-
-insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
-values ('PUSH', 'RISK PUSH', 'S', 'S', 'bQvry6WjGfCSg3R3K3zOogGl9XVhISEmqu9aZyMIK/0=', 'Servicio para envío de mensajes a los usuarios a través de Notificaciones push', '0.1', 86400, 10000, null, null);
-
-insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
-values ('SMS', 'RISK SMS', 'S', 'S', 'Va1eggcdH9F27TTuLFMzU6s4mt568v439ZOD+8dw9ig=', 'Servicio para envío de mensajes a los usuarios a través de Mensaje de texto (SMS)', '0.1', 86400, 10000, null, null);
+values ('MSJ', 'RISK MSJ', 'S', 'S', 'Hkru7cXdu6FdIqhA9Q4XJqTgW/pW651rDjlSyLczKwI=', 'Servicio para envío de mensajes a los usuarios a través de Correo electrónico (E-mail), Mensaje de texto (SMS) y Notificación push', '0.1', 86400, 10000, null, null);
 
 insert into t_aplicaciones (ID_APLICACION, NOMBRE, TIPO, ACTIVO, CLAVE, DETALLE, VERSION_ACTUAL, TIEMPO_EXPIRACION_ACCESS_TOKEN, TIEMPO_EXPIRACION_REFRESH_TOKEN, TEMPLATE_NOTIFICACION, PLATAFORMA_NOTIFICACION)
 values ('TEST', 'RISK TEST', 'W', 'S', 'KM1gROAnPUAZykrHcOOJRAC1jkcnyBVLRWXcRD3pjvQ=', 'Cliente HTTP para pruebas de Servicios Web', '0.1', 3600, 5, null, null);
