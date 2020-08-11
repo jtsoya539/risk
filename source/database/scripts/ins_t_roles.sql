dae014b8a45aaff930775fac6af75d7143d08539
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2020-08-11 11:41:42 -0400

    Ajustar detalle de rol

--- source/database/scripts/ins_t_roles.sql
+++ source/database/scripts/ins_t_roles.sql
@@ -8,6 +8,6 @@ insert into t_roles (ID_ROL, NOMBRE, ACTIVO, DETALLE)
 values (2, 'USUARIO', 'S', null);
 
 insert into t_roles (ID_ROL, NOMBRE, ACTIVO, DETALLE)
-values (3, 'MENSAJERIA', 'S', 'Rol para envío de mensajes a los usuarios a través de Correo electrónico (E-mail) y Mensaje de texto (SMS)');
+values (3, 'MENSAJERIA', 'S', 'Rol para envío de mensajes a los usuarios a través de Correo electrónico (E-mail), Mensaje de texto (SMS) y Notificación push');
 
 prompt Done.
