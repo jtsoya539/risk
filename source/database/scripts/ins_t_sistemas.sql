d2e017b7c739f787cdc8064e3e05b88608ffa887
jtsoya539 <javier.meza.py@gmail.com>
2020-10-13 16:59:56 -0300

    Actualizar fecha de versión 0.1.0

--- source/database/scripts/ins_t_sistemas.sql
+++ source/database/scripts/ins_t_sistemas.sql
@@ -1,7 +1,8 @@
 prompt Importing table t_sistemas...
 set feedback off
 set define off
+
 insert into t_sistemas (ID_SISTEMA, NOMBRE, DETALLE, ACTIVO, FECHA_ACTUAL, VERSION_ACTUAL)
-values ('RISK', 'RISK', null, 'S', to_date('09-09-2020', 'dd-mm-yyyy'), '0.1.0');
+values ('RISK', 'RISK', null, 'S', to_date('13-10-2020', 'dd-mm-yyyy'), '0.1.0');
 
 prompt Done.
