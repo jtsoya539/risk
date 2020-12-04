6771e4c4007355e0d73d04d3ab7865165ebbfdca
jtsoya539 <javier.meza.py@gmail.com>
2020-12-04 14:16:10 -0300

    Agregar logging a Risk.Msj

--- source/backend/Risk.Msj/Program.cs
+++ source/backend/Risk.Msj/Program.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace Risk.Msj
 {
@@ -14,7 +12,21 @@ namespace Risk.Msj
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(Path.Combine("config", "log", "nlog.config")).GetCurrentClassLogger();
+            try
+            {
+                logger.Info("Iniciando Risk.Msj");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Fatal(exception, "Error al iniciar Risk.Msj");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -28,6 +40,12 @@ namespace Risk.Msj
                     services.AddHostedService<WorkerMail>();
                     services.AddHostedService<WorkerPush>();
                     services.AddHostedService<WorkerSMS>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
