48e0d5e9e80a372f4e624f6ca992720131fca27b
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2021-01-06 13:21:43 -0300

    Cambiar namespace a Risk.API.Mappers

--- source/backend/Risk.API/Mappers/ModelsMapper.cs
+++ source/backend/Risk.API/Mappers/ModelsMapper.cs
@@ -26,7 +26,7 @@ using System.Collections.Generic;
 using Risk.API.Entities;
 using Risk.API.Models;
 
-namespace Risk.API.Helpers
+namespace Risk.API.Mappers
 {
     public static class ModelsMapper
     {
