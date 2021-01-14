3927ff38816fca50532cd60e88860739b800a57f
jtsoya539 <javier.meza.py@gmail.com>
2021-01-14 19:22:19 -0300

    Actualizar versión del sistema v0.3.0

--- source/database/scripts/ins_t_sistemas.sql
+++ source/database/scripts/ins_t_sistemas.sql
@@ -3,6 +3,6 @@ set feedback off
 set define off
 
 insert into t_sistemas (ID_SISTEMA, NOMBRE, DETALLE, ACTIVO, FECHA_ACTUAL, VERSION_ACTUAL)
-values ('RISK', 'RISK', null, 'S', to_date('16-12-2020', 'dd-mm-yyyy'), '0.2.0');
+values ('RISK', 'RISK', null, 'S', to_date('14-01-2021', 'dd-mm-yyyy'), '0.3.0');
 
 prompt Done.
