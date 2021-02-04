d81b04ecc67d6c27945275262c2fd34dc5a80775
javier.meza.py@gmail.com <javier.meza.py@gmail.com>
2021-02-04 16:05:02 -0300

    Agregar nuevos mensajes de error en tabla de errores

--- source/database/scripts/ins_t_errores.sql
+++ source/database/scripts/ins_t_errores.sql
@@ -1,6 +1,23 @@
 prompt Importing table t_errores...
 set feedback off
 set define off
+insert into t_errores (ID_ERROR, MENSAJE)
+values ('ora0001', 'Valor no permitido');
+
+insert into t_errores (ID_ERROR, MENSAJE)
+values ('ora0002', 'Tipo de dato de @1@ @2@ no soportado');
+
+insert into t_errores (ID_ERROR, MENSAJE)
+values ('ora0003', 'Parámetro @1@ obligatorio');
+
+insert into t_errores (ID_ERROR, MENSAJE)
+values ('ora0004', 'Parámetro @1@ debe tener valor');
+
+insert into t_errores (ID_ERROR, MENSAJE)
+values ('ora0005', 'Parámetro @1@ de tipo incorrecto');
+
+insert into t_errores (ID_ERROR, MENSAJE)
+values ('ora0006', 'Longitud del parámetro @1@ no debe ser superior a @2@');
 
 insert into t_errores (ID_ERROR, MENSAJE)
 values ('ser0002', 'Error al procesar parámetros del servicio');
