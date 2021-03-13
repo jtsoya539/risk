aef6b87a0f01f04cac474d1dd06534d39edf9e49
dmezac <damian.meza.py@hotmail.com>
2021-03-12 22:32:13 -0300

    Agregar permisos para rol de usuario nuevo en el api.-

--- source/backend/Risk.API/Controllers/AutController.cs
+++ source/backend/Risk.API/Controllers/AutController.cs
@@ -39,7 +39,7 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace Risk.API.Controllers
 {
     [SwaggerTag("Servicios del dominio AUTENTICACIÓN", "https://jtsoya539.github.io/risk/")]
-    [Authorize(Roles = "ADMINISTRADOR,USUARIO")]
+    [Authorize(Roles = "ADMINISTRADOR,USUARIO,USUARIO_NUEVO")]
     [Route("Api/[controller]")]
     [ApiController]
     public class AutController : RiskControllerBase
