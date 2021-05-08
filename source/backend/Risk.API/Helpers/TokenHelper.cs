c248947a40b64e9c1e4b4e88a1c2a802515c911f
dmezac <damian.meza.py@hotmail.com>
2021-05-08 09:46:34 -0400

    Ajuste en validación de emisor de token de google.-

--- source/backend/Risk.API/Helpers/TokenHelper.cs
+++ source/backend/Risk.API/Helpers/TokenHelper.cs
@@ -209,7 +209,7 @@ namespace Risk.API.Helpers
 
                 // Validamos el emisor del token
                 var respValorEmisor = genService.ValorParametro("GOOGLE_EMISOR_TOKEN");
-                if (!respValorEmisor.Codigo.Equals(RiskConstants.CODIGO_OK) || respValorEmisor.Datos.Contenido != emisorToken)
+                if (!respValorEmisor.Codigo.Equals(RiskConstants.CODIGO_OK) || !respValorEmisor.Datos.Contenido.Contains(emisorToken, StringComparison.OrdinalIgnoreCase))
                     throw new SecurityTokenValidationException("Emisor de token no válido.");
 
                 // Validamos el cliente del token
