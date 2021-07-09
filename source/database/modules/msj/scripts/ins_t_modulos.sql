27ac9c7b2ac69779e24098405b606f516701450a
Javier Meza <javier.meza.py@gmail.com>
2021-07-09 11:47:36 -0400

    Actualizar versión de módulos v0.10.0

--- source/database/modules/msj/scripts/ins_t_modulos.sql
+++ source/database/modules/msj/scripts/ins_t_modulos.sql
@@ -1,8 +1,7 @@
 prompt Importing table t_modulos...
 set feedback off
 set define off
-
 insert into t_modulos (ID_MODULO, NOMBRE, DETALLE, ACTIVO, FECHA_ACTUAL, VERSION_ACTUAL)
-values ('MSJ', 'MENSAJERÍA', 'Módulo para envío de mensajes a los usuarios a través de Correo electrónico (E-mail), Mensaje de texto (SMS) y Notificación push', 'S', to_date('10-05-2021', 'dd-mm-yyyy'), '0.9.0');
+values ('MSJ', 'MENSAJERÍA', 'Módulo para envío de mensajes a los usuarios a través de Correo electrónico (E-mail), Mensaje de texto (SMS) y Notificación push', 'S', to_date('09-07-2021', 'dd-mm-yyyy'), '0.10.0');
 
 prompt Done.
--- source/database/scripts/ins_t_modulos.sql
+++ source/database/scripts/ins_t_modulos.sql
@@ -1,8 +1,7 @@
 prompt Importing table t_modulos...
 set feedback off
 set define off
-
 insert into t_modulos (ID_MODULO, NOMBRE, DETALLE, ACTIVO, FECHA_ACTUAL, VERSION_ACTUAL)
-values ('RISK', 'RISK', 'Módulo principal con manejo de Operaciones (Servicios Web, Reportes, Trabajos) y sus Parámetros, Autenticación y Autorización de Usuarios, Parámetros, Significados, Archivos, Datos Adicionales, entre otros', 'S', to_date('10-05-2021', 'dd-mm-yyyy'), '0.9.0');
+values ('RISK', 'RISK', 'Módulo principal con manejo de Operaciones (Servicios Web, Reportes, Trabajos) y sus Parámetros, Autenticación y Autorización de Usuarios, Parámetros, Significados, Archivos, Datos Adicionales, entre otros', 'S', to_date('09-07-2021', 'dd-mm-yyyy'), '0.10.0');
 
 prompt Done.
