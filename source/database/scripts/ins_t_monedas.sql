2aa676da4864b40b22ec7969a7a92bbee0f4a00b
Javier Meza <javier.meza.py@gmail.com>
2021-08-30 15:20:27 -0400

    Ajustes en tabla de monedas

--- source/database/scripts/ins_t_monedas.sql
+++ source/database/scripts/ins_t_monedas.sql
@@ -1,14 +1,19 @@
 prompt Importing table t_monedas...
 set feedback off
 set define off
+insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS, CODIGO_ISO)
+values ('ARP', 'PESO ARGENTINO', 'S', '999G999G999G999G999D00', null, 12, 'ARS');
 
-insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS)
-values ('EUR', 'EUROS', 'S', '999G999G999G999G999D00', '€', null);
+insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS, CODIGO_ISO)
+values ('EUR', 'EURO', 'S', '999G999G999G999G999D00', null, null, 'EUR');
 
-insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS)
-values ('GS', 'GUARANÍES', 'S', '999G999G999G999G999', 'G', 177);
+insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS, CODIGO_ISO)
+values ('GS', 'GUARANÍ', 'S', '999G999G999G999G999', null, 177, 'PYG');
 
-insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS)
-values ('USD', 'DÓLARES AMERICANOS', 'S', '999G999G999G999G999D00', '$', 65);
+insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS, CODIGO_ISO)
+values ('RS', 'REAL BRASILEÑO', 'S', '999G999G999G999G999D00', null, 31, 'BRL');
+
+insert into t_monedas (ID_MONEDA, DESCRIPCION, ACTIVO, FORMATO, SIMBOLO, ID_PAIS, CODIGO_ISO)
+values ('USD', 'DÓLAR ESTADOUNIDENSE', 'S', '999G999G999G999G999D00', '$', 65, 'USD');
 
 prompt Done.
