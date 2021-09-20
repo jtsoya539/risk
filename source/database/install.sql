f18f0f556910552cf1b01b769ac2c8397dd3770d
Javier Meza <javier.meza.py@gmail.com>
2021-09-20 09:31:23 -0400

    Ajustar orden de instalación y desinstalación de paquetes de BD

--- source/database/install.sql
+++ source/database/install.sql
@@ -151,12 +151,12 @@ prompt
 @@packages/k_auditoria.pck
 @@packages/k_archivo.pck
 @@packages/k_html.pck
+@@packages/k_dato.pck
 @@packages/k_aplicacion.pck
 @@packages/k_usuario.pck
 @@packages/k_dispositivo.pck
 @@packages/k_sesion.pck
 @@packages/k_error.pck
-@@packages/k_dato.pck
 @@packages/k_autenticacion.pck
 @@packages/k_autorizacion.pck
 @@packages/k_operacion.pck
