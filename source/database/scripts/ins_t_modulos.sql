4ce0cd992309499f76bee6f15798106f6bf3f38e
jtsoya539 <javier.meza.py@gmail.com>
2021-11-01 17:24:39 -0300

    Actualizar versión de módulos v1.1.0

--- source/database/scripts/ins_t_modulos.sql
+++ source/database/scripts/ins_t_modulos.sql
@@ -1,7 +1,8 @@
 prompt Importing table t_modulos...
 set feedback off
 set define off
+
 insert into t_modulos (ID_MODULO, NOMBRE, DETALLE, ACTIVO, FECHA_ACTUAL, VERSION_ACTUAL)
-values ('RISK', 'RISK', 'Módulo principal con manejo de Operaciones (Servicios Web, Reportes, Trabajos) y sus Parámetros, Autenticación y Autorización de Usuarios, Parámetros, Significados, Archivos, Datos Adicionales, entre otros', 'S', to_date('09-07-2021', 'dd-mm-yyyy'), '0.10.0');
+values ('RISK', 'RISK', 'Módulo principal con manejo de Operaciones (Servicios Web, Reportes, Trabajos) y sus Parámetros, Autenticación y Autorización de Usuarios, Parámetros, Significados, Archivos, Datos Adicionales, entre otros', 'S', to_date('01-11-2021', 'dd-mm-yyyy'), '1.1.0');
 
 prompt Done.
