e8bab776ac4e031d30b91dab0e8fe0691f2a2b17
Javier Meza <javier.meza.py@gmail.com>
2022-05-10 09:29:29 -0400

    Agregar descripción y detalle de dominios de significados

--- source/database/modules/msj/scripts/ins_t_significado_dominios.sql
+++ source/database/modules/msj/scripts/ins_t_significado_dominios.sql
@@ -2,12 +2,12 @@ prompt Importing table t_significado_dominios...
 set feedback off
 set define off
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ESTADO_MENSAJERIA', 'ESTADO_MENSAJERIA', null, 'S', 'MSJ');
+values ('ESTADO_MENSAJERIA', 'Estados de mensajerías', null, 'S', 'MSJ');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('EXTENSION_ADJUNTO', 'EXTENSION_ADJUNTO', null, 'S', 'MSJ');
+values ('EXTENSION_ADJUNTO', 'Extensiones permitidas para archivos adjuntos de Correos electrónicos (E-mail)', 'El valor de la referencia indica el tipo MIME relacionado a la extensión', 'S', 'MSJ');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_MENSAJERIA', 'TIPO_MENSAJERIA', null, 'S', 'MSJ');
+values ('TIPO_MENSAJERIA', 'Tipos de mensajerías', null, 'S', 'MSJ');
 
 prompt Done.
--- source/database/scripts/ins_t_significado_dominios.sql
+++ source/database/scripts/ins_t_significado_dominios.sql
@@ -2,63 +2,63 @@ prompt Importing table t_significado_dominios...
 set feedback off
 set define off
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ACCION_PERMISO', 'ACCION_PERMISO', null, 'S', 'AUT');
+values ('ACCION_PERMISO', 'Acciones que pueden realizarse sobre un permiso', null, 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ESTADO_CLAVE', 'ESTADO_CLAVE', null, 'S', 'AUT');
+values ('ESTADO_CLAVE', 'Estados de claves', null, 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ESTADO_SESION', 'ESTADO_SESION', null, 'S', 'AUT');
+values ('ESTADO_SESION', 'Estados de sesiones', null, 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ESTADO_USUARIO', 'ESTADO_USUARIO', null, 'S', 'GEN');
+values ('ESTADO_USUARIO', 'Estados de usuarios', 'El valor de la referencia indica el nombre del rol que se agrega por defecto a un usuario cuando se registra', 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('EXTENSION_FUENTE', 'EXTENSION_FUENTE', null, 'S', 'GEN');
+values ('EXTENSION_FUENTE', 'Extensiones permitidas para archivos de fuentes', 'El valor de la referencia indica el tipo MIME relacionado a la extensión', 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('EXTENSION_IMAGEN', 'EXTENSION_IMAGEN', null, 'S', 'GEN');
+values ('EXTENSION_IMAGEN', 'Extensiones permitidas para archivos de imágenes', 'El valor de la referencia indica el tipo MIME relacionado a la extensión', 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('EXTENSION_REPORTE', 'EXTENSION_REPORTE', null, 'S', 'GEN');
+values ('EXTENSION_REPORTE', 'Extensiones permitidas para archivos de reportes', 'El valor de la referencia indica el tipo MIME relacionado a la extensión', 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ORIGEN_SESION', 'ORIGEN_SESION', null, 'S', 'AUT');
+values ('ORIGEN_SESION', 'Orígenes de sesiones', null, 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('ORIGEN_USUARIO', 'ORIGEN_USUARIO', null, 'S', 'GEN');
+values ('ORIGEN_USUARIO', 'Orígenes de usuarios', null, 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('POLITICA_VALIDACION_CLAVE_ACCESO', 'POLITICA_VALIDACION_CLAVE_ACCESO', null, 'S', 'AUT');
+values ('POLITICA_VALIDACION_CLAVE_ACCESO', 'Políticas para validación de claves de acceso', 'El valor de la referencia indica el tipo de clave', 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('POLITICA_VALIDACION_CLAVE_TRANSACCIONAL', 'POLITICA_VALIDACION_CLAVE_TRANSACCIONAL', null, 'S', 'AUT');
+values ('POLITICA_VALIDACION_CLAVE_TRANSACCIONAL', 'Políticas para validación de claves transaccionales', 'El valor de la referencia indica el tipo de clave', 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_APLICACION', 'TIPO_APLICACION', null, 'S', 'GEN');
+values ('TIPO_APLICACION', 'Tipos de aplicaciones', null, 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_CLAVE', 'TIPO_CLAVE', null, 'S', 'AUT');
+values ('TIPO_CLAVE', 'Tipos de claves', null, 'S', 'AUT');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_DATO_PARAMETRO', 'TIPO_DATO_PARAMETRO', null, 'S', 'API');
+values ('TIPO_DATO_PARAMETRO', 'Tipos de dato de parámetros', null, 'S', 'API');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_DISPOSITIVO', 'TIPO_DISPOSITIVO', null, 'S', 'GEN');
+values ('TIPO_DISPOSITIVO', 'Tipos de dispositivos', null, 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_OPERACION', 'TIPO_OPERACION', null, 'S', 'API');
+values ('TIPO_OPERACION', 'Tipos de operaciones', null, 'S', 'API');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_PERSONA', 'TIPO_PERSONA', null, 'S', 'GEN');
+values ('TIPO_PERSONA', 'Tipos de personas', null, 'S', 'GEN');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_REPORTE', 'TIPO_REPORTE', null, 'S', 'API');
+values ('TIPO_REPORTE', 'Tipos de reportes', null, 'S', 'API');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_SERVICIO', 'TIPO_SERVICIO', null, 'S', 'API');
+values ('TIPO_SERVICIO', 'Tipos de servicios', null, 'S', 'API');
 
 insert into t_significado_dominios (DOMINIO, DESCRIPCION, DETALLE, ACTIVO, ID_DOMINIO)
-values ('TIPO_IMPLEMENTACION', 'TIPO_IMPLEMENTACION', null, 'S', 'API');
+values ('TIPO_IMPLEMENTACION', 'Tipos de implementación de operaciones', 'El valor de la referencia se utiliza para armar el nombre del programa a ejecutar para la operación. Ver la función k_operacion.f_nombre_programa', 'S', 'API');
 
 prompt Done.
