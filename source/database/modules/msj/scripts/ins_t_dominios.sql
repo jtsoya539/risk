e7302340f1648e8a33271f367b02c358f398958a
Javier Meza <javier.meza@atlas.com.py>
2024-03-11 16:42:29 -0300

    Agregar detalles de dominios

--- source/database/modules/msj/scripts/ins_t_dominios.sql
+++ source/database/modules/msj/scripts/ins_t_dominios.sql
@@ -2,6 +2,6 @@ prompt Importing table t_dominios...
 set feedback off
 set define off
 insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
-values ('MSJ', 'MENSAJERÍA', null, 'S', 'MSJ');
+values ('MSJ', 'MENSAJERÍA', 'Dominio de Mensajería', 'S', 'MSJ');
 
 prompt Done.
--- source/database/scripts/ins_t_dominios.sql
+++ source/database/scripts/ins_t_dominios.sql
@@ -1,17 +1,16 @@
 prompt Importing table t_dominios...
 set feedback off
 set define off
+insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
+values ('GEN', 'GENERAL', 'Dominio General', 'S', 'RISK');
 
 insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
-values ('GEN', 'GENERAL', null, 'S', 'RISK');
+values ('AUT', 'AUTENTICACIÓN', 'Dominio de Autenticación y Autorización de Usuarios', 'S', 'RISK');
 
 insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
-values ('AUT', 'AUTENTICACIÓN', null, 'S', 'RISK');
+values ('API', 'OPERACIONES', 'Dominio de Operaciones (Servicios Web, Reportes, Trabajos) y sus Parámetros', 'S', 'RISK');
 
 insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
-values ('API', 'SERVICIO', null, 'S', 'RISK');
-
-insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
-values ('GLO', 'GLOBAL', null, 'S', 'RISK');
+values ('GLO', 'GLOBAL', 'Dominio Global', 'S', 'RISK');
 
 prompt Done.
