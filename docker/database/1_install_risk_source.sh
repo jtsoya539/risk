073090f4048ae2eb494110dbca62a470ba5320a4
Javier Meza <javier.meza.py@gmail.com>
2025-03-22 20:26:37 -0300

    Ajustes en script de instalación de Docker

--- docker/database/1_install_risk_source.sh
+++ docker/database/1_install_risk_source.sh
@@ -6,11 +6,6 @@ echo "BUILDER: Source installation started"
 export SQLPATH="/usr/src/risk/source/:$SQLPATH"
 sqlplus sys/$ORACLE_PASSWORD@//localhost/XEPDB1 as sysdba @install_headless.sql $RISK_DB_USER $RISK_DB_PASSWORD
 
-# Install modules
-export SQLPATH="/usr/src/risk/source/modules/msj/:$SQLPATH"
-sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 install_dependencies.sql
-sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 @install.sql
-
 # Install migrations
 export SQLPATH="/usr/src/risk/source/migrations/mig_000001/:$SQLPATH"
 sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 @install.sql
@@ -19,4 +14,10 @@ export SQLPATH="/usr/src/risk/source/migrations/mig_000002/:$SQLPATH"
 sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 @install.sql
 
 export SQLPATH="/usr/src/risk/source/migrations/mig_000003/:$SQLPATH"
+sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 @install.sql
+
+# Install modules
+# msj
+export SQLPATH="/usr/src/risk/source/modules/msj/:$SQLPATH"
+sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 @install_dependencies.sql
 sqlplus $RISK_DB_USER/$RISK_DB_PASSWORD@//localhost/XEPDB1 @install.sql
